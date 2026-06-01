c8f63e5a44c7c5732600d0d6025408a8d29f15cd
Erick Lievana Poy <ericklievana@gmail.com>
2026-06-01 03:14:49 -0600

    FIX not able to move floating window
    Error was due to the loading of some xresources values

--- config.h
+++ config.h
@@ -57,13 +57,6 @@ ResourcePref resources[] = {
   { "selbgcolor",         STRING,  &selbgcolor },
   { "selbordercolor",     STRING,  &selbordercolor },
   { "selfgcolor",         STRING,  &selfgcolor },
-  { "borderpx",          	INTEGER, &borderpx },
-  { "snap",          		INTEGER, &snap },
-  { "showbar",          	INTEGER, &showbar },
-  { "topbar",          	INTEGER, &topbar },
-  { "nmaster",          	INTEGER, &nmaster },
-  { "resizehints",       	INTEGER, &resizehints },
-  { "mfact",      	 	FLOAT,   &mfact },
 };
 
 
